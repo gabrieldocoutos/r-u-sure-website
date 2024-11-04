d2da2ca073dc62342583d21eac7908039f6eaa9d
Gabriel D. Couto <gabrieldocoutos@gmail.com>
2024-11-03 21:15:54 -0300

    foo

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 
-
 const callbackOrigin = new URLSearchParams(window.location.search).get("callback")
 
 document.querySelector("#content").textContent = `r u sure u want to go to ${callbackOrigin}`
