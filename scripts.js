296fa8a0298799affa25199b8a5866fa1e3e6bb9
Gabriel D. Couto <gabrieldocoutos@gmail.com>
2024-11-03 22:55:31 -0300

    wip

--- scripts.js
+++ scripts.js
@@ -4,9 +4,6 @@ const callbackOrigin = new URLSearchParams(window.location.search).get("callback
 document.querySelector("#content").textContent = `r u sure u want to go to ${callbackOrigin}`
 
 
-
-
-
 function handleYes() {
     window.location.href = `${callbackOrigin}?rusure=allow`;
 }
